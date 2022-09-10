fdc81a00345a9ab840233f147a1ba28fcfe7eca6
PHUONG.DD194141@sis.hust.edu.vn <PHUONG.DD194141@sis.hust.edu.vn>
2022-09-10 15:16:15 +0700

    add Study and Attendance migration

---
+++ database/migrations/2022_09_10_144150_create_studies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('studies', function (Blueprint $table) {
+            $table->id();
+            $table->integer('class_id');
+            $table->integer('lesson_id');
+            $table->string('teacher', 255);
+            $table->string('coach', 255);
+            $table->timestamp('starttime');
+            $table->string('location')->nullable();
+            $table->integer('created_by')->default(0);
+            $table->integer('updated_by')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('studies');
+    }
+}
---
+++ database/migrations/2022_09_10_144955_create_attendances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->integer('study_id');
+            $table->integer('student_id');
+            $table->tinyInteger('status');
+            $table->string('note', 255)->nullable();
+            $table->integer('created_by')->default(0);
+            $table->integer('updated_by')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
